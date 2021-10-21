2f150af815c3b208a99d1100550c8d12ce5bc08f
David Pierre <davidpierrealves21@gmail.com>
2021-10-21 15:26:20 -0300

    Add language mapping to automatically set syntax highlighting

--- src/renderers/Code.svelte
+++ src/renderers/Code.svelte
@@ -1,15 +1,42 @@
 <script>
-  import { HighlightAuto } from "svelte-highlight";
-  import atlas from "svelte-highlight/src/styles/atlas";
+  import { Highlight } from 'svelte-highlight'
+  import atlas from 'svelte-highlight/src/styles/atlas'
+  import {
+    c,
+    python,
+    django,
+    javascript,
+    ruby,
+    go,
+    rust,
+    shell,
+    css,
+    scss,
+    yaml,
+    json,
+  } from 'svelte-highlight/src/languages'
+
+  const LANGUAGES = {
+    c: c,
+    python: python,
+    django: django,
+    javascript: javascript,
+    ruby: ruby,
+    go: go,
+    rust: rust,
+    shell: shell,
+    css: css,
+    scss: scss,
+    yaml: yaml,
+    json: json,
+  }
 
   export let lang
   export let text
 </script>
 
-
 <svelte:head>
   {@html atlas}
 </svelte:head>
 
-
-<HighlightAuto code="{text}" langtag="{true}"/>
+<Highlight code={text} language={LANGUAGES[lang]} />
